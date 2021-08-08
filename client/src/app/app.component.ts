f74750938ec16021f160ad3dc44ad05eddc28714
Theo <theo.poette@epitech.eu>
2021-08-08 23:48:49 +0200

    feat: popover plus start of category buttons

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { HttpErrorResponse } from '@angular/common/http';
-import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, SimpleChanges, ElementRef } from '@angular/core';
 import { Item } from './Item';
 import { ItemService } from './item.service';
 import { FormHandlerService } from './form-handler.service';
@@ -7,10 +7,14 @@ import { FormHandlerService } from './form-handler.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  host: {
+    '(document:click)': 'onPageClick($event)'
+  }
 })
 export class AppComponent implements OnInit {
   public items: Item[] = [];
+  public actionsRequest: boolean = false;
 
   constructor(private ItemService: ItemService, private FormService: FormHandlerService) {}
 
@@ -20,6 +24,30 @@ export class AppComponent implements OnInit {
 
   triggerRefresh(): void {
     this.getItems();
+  }
+
+  public onPageClick(event: Event): void {
+    if (!this.actionsRequest)
+      return;
+    const target = event.target as HTMLElement;
+    if (target) {
+      if (target.id !== "popover" && target.id !== "moreActions") {
+        this.actionsRequest = false;
+        document.getElementById("popover")?.classList?.add("hidden");
+      }
+    }
+  }
+
+  public itemActions(element: Event, item: Item): void {
+    this.actionsRequest = true;
+    const target = element.target as HTMLElement;
+    let {x, y} = target.getBoundingClientRect();
+    let popover = document.getElementById("popover");
+    if (!popover)
+      return;
+    popover.classList.remove("hidden");
+    popover.style.left = x + 'px';
+    popover.style.top = y + 'px';
   }
 
   public getItems(): void {
