408a29de5e29d203fef21b81545f90a8f4eb0a2c
Theo <theo.poette@epitech.eu>
2021-08-09 15:05:57 +0200

    fix: removed missing package

--- server/src/main/java/com/manager/item_manager/ItemManagerApplication.java
+++ server/src/main/java/com/manager/item_manager/ItemManagerApplication.java
@@ -1,16 +1,11 @@
 package com.manager.item_manager;
 
 import com.manager.item_manager.repository.ItemRepository;
-import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
-import com.manager.item_manager.model.Item;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ItemManagerApplication implements CommandLineRunner {
